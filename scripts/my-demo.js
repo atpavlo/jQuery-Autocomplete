982a6f44c938af1bab530065b09d09432502bc66
Paul <atpavlo@gmail.com>
2022-05-26 18:01:03 +0300

    Test init

---
+++ scripts/my-demo.js
@@ -0,0 +1,18 @@
+﻿/*jslint  browser: true, white: true, plusplus: true */
+/*global $, countries */
+
+$(function () {
+    'use strict';
+    var countries = [
+        { value: 'Andorra', data: 'AD' },
+        { value: 'Zimbabwe', data: 'ZZ' },
+        { value: 'Ukraine', data: 'UA' }
+    ];
+
+    $('#autocomplete').autocomplete({
+        lookup: countries,
+        onSelect: function (suggestion) {
+            alert('You select: ' + suggestion.value + '' + suggestion.data );
+        }
+    })
+});
